a9ffd367818e20328582489edc257baeef3e8bf2
Bekham <anton-bekker@yandex.ru>
2021-03-07 00:11:06 +0600

    Task 3

---
+++ Task_5_3.py
@@ -0,0 +1,12 @@
+from itertools import zip_longest
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А'
+]
+answer = zip_longest(tutors, klasses)
+for i in answer:
+    print(i)
